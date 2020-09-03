f7997df4dd6e99ff3d04cf6f8a655b288aaa06af
Talha112 <talhardahan@gmail.com>
2020-09-03 18:53:36 +0300

    Updated source code to includethe comments

--- Assignment.java
+++ Assignment.java
@@ -1,8 +1,8 @@
 public class Assignment {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {      //The main method
 
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");     //Hello World message being printed ot to the console
 
     }
 }
